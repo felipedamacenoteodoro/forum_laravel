2bb2f15904e9ca6c7ebadd2c7aa4647faf675a4a
Felipe D. Teodoro <felipe.teodoro@abgf.gov.br>
2019-03-18 14:23:09 -0300

    [ADD] Create migration and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\User;
+use App\Thread;
+use App\Reply;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -22,5 +24,27 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Thread::class, function ($faker){
+    return [
+      'user_id' => function (){
+        return factory('App\User')->create()->id;
+      },
+      'title' => $faker->sentence,
+      'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(Reply::class, function ($faker){
+    return [
+      'thread_id' => function (){
+        return factory('App\Thread')->create()->id;
+      },
+      'user_id' => function (){
+        return factory('App\User')->create()->id;
+      },
+      'body' => $faker->paragraph
     ];
 });
